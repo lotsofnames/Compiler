c61da31f6a6dcc79d06f1c73c04144fa5576f391
matus <matussadovsky99@gmail.com>
2025-06-05 18:47:58 +0200

    Ayri, keď ťa Boh tvoril, asi si bol v beta verzii. A bugy tam zostali dodnes.

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ def g64(x, z, h, f, X, Z, file):
         file.write(f"G01 X{X} Z{z} F{f}\n")
         file.write(f"G00 X{X+2} Z{Z}\n")
 
-    print(x, z, h, f, X, Z)
 
 
 lines = []
@@ -30,11 +29,22 @@ with open("CNC.txt", "r") as file:
         lines.append(line)
 
 with open("compliedCNC.txt", "w") as file:
-
+    file.write("G54\n")
     for line in lines:
         ok = 0
+        if "m03" in line.lower().strip() or "m3" in line.lower().strip():
+            M3=line.strip().lower().split(" ")
 
-        if "G01" in line or "G00" in line or "G0" in line or "G1" in line:
+            for i in M3:
+
+                if i.startswith("s"):
+                    file.write(f"{i.title()} ")
+                elif i.startswith("m"):
+                    file.write(f"{i.replace('0', '').title()} ")
+                else:
+                    continue
+            file.write("\n")
+        elif "G01" in line or "G00" in line or "G0" in line or "G1" in line:
             file.write(line)
 
             XS = line.split(" ")
@@ -71,5 +81,7 @@ with open("compliedCNC.txt", "w") as file:
             if ok < 4 or ok > 5 and Z != None and X != None:
                 sys.exit("ok error")
             g64(x, z, h, f, X, Z, file)
+            X=x
+            Z=z
         else:
             file.write(line)
