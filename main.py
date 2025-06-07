ec01f526b29b62e3cde58bd507b5be1ba236a87c
matus <matussadovsky99@gmail.com>
2025-06-07 12:01:54 +0200

    Keď Ayri niečo povie, aj ticho znie ako múdrejší nápad.

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def g64(x, z, h, f, X, Z, file):
         file.write(f"G00 X{X+2} Z{Z}\n")
 
 
-
 lines = []
 with open("CNC.txt", "r") as file:
     for line in file:
@@ -30,15 +29,30 @@ with open("CNC.txt", "r") as file:
 
 with open("compliedCNC.txt", "w") as file:
     file.write("G54\n")
+
     for line in lines:
+        k = []
         ok = 0
+        for li in line:
+            if li.isnumeric():
+                continue
+            elif li == " " or li == "\n " or li == "\n":
+                continue
+            elif li == "." or li == "-" or li == "+" or li == "*" or li == "/":
+                continue
+            else:
+                k.append(f" {li}")
+        for o in k:
+            line = line.replace(o.strip(), o)
+        line = line.strip()
         while 1:
             if "  " in line:
-                line = line.replace("  ", " ")
+                line = line.replace("  ", " ").strip()
             else:
                 break
+
         if "m03" in line.lower().strip() or "m3" in line.lower().strip():
-            M3=line.strip().lower().split(" ")
+            M3 = line.strip().lower().split(" ")
 
             for i in M3:
 
@@ -50,7 +64,7 @@ with open("compliedCNC.txt", "w") as file:
                     continue
             file.write("\n")
         elif "G01" in line or "G00" in line or "G0" in line or "G1" in line:
-            file.write(line)
+            file.write(f"{line}\n")
 
             XS = line.split(" ")
             for xs in XS:
@@ -86,7 +100,7 @@ with open("compliedCNC.txt", "w") as file:
             if ok < 4 or ok > 5 and Z != None and X != None:
                 sys.exit("ok error")
             g64(x, z, h, f, X, Z, file)
-            X=x
-            Z=z
+            X = x
+            Z = z
         else:
-            file.write(line)
+            file.write(f"{line}\n")
